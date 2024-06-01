dcbcbbf72870d06eead379c6abc1c7d95997d3d0
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-05-31 18:11:31 -0700

    earning dialog

--- components/shared/EarningDialog.tsx
+++ components/shared/EarningDialog.tsx
@@ -44,48 +44,68 @@ const EarningDialog = ({ earning }: { earning: IEarning }) => {
                         <AlertDialogCancel className="rounded-full bg-white text-[#1AAD7A] hover:bg-black hover:text-white">X</AlertDialogCancel>
                     </AlertDialogTitle>
                 </AlertDialogHeader>
-                <table className='w-full text-[13px] md:text-[16px]'>
+                <table className='w-full text-[13px] md:text-[16px] border-collapse border border-[#1AAD7A]'>
                     <tbody>
                         <tr>
-                            <td className='font-bold pr-2 bg-white rounded-tl-lg border-b-[3px] border-[#1AAD7A] p-2'>Order ID:</td>
-                            <td className='font-bold text-black bg-white border-b-[3px] border-l-[3px] border-[#1AAD7A] rounded-tr-lg p-2'>{earning?._id}</td>
+                            <td className='font-bold pr-2 bg-white border-[#1AAD7A] border p-2 rounded-tl-lg border-b-[3px] border-r-[3px]'>Order ID:</td>
+                            <td className='font-bold text-black bg-white border-[#1AAD7A] border p-2 rounded-tr-lg border-b-[3px]'>{earning?._id}</td>
                         </tr>
                         <tr>
-                            <td className='font-bold pr-2 bg-white border-b-[3px] border-[#1AAD7A] p-2'>Serivce:</td>
-                            <td className='font-bold text-black bg-white border-b-[3px] border-l-[3px] border-[#1AAD7A] p-2 flex flex-row items-center'>
-                                {earning.service == 'TextReview' &&
-                                    <div className='w-full flex flex-row items-center gap-2'>
-                                        <Image src={'/icons/star-white.svg'} alt='video' width={200} height={200} className='bg-blue-500 w-[25px] h-[25px] md:w-[30px] md:h-[30px] p-[3px] rounded-full' />
+                            <td className='font-bold pr-2 bg-white border-[#1AAD7A] border p-2 border-b-[3px] border-r-[3px]'>Service:</td>
+                            <td className='font-bold text-black bg-white border-[#1AAD7A] border p-2 border-b-[3px]'>
+                                {earning.service === 'TextReview' && (
+                                    <div className='flex flex-row items-center gap-2'>
+                                        <Image src={'/icons/star-white.svg'} alt='text review' width={25} height={25} className='bg-blue-500 p-[3px] rounded-full' />
                                         <p className='font-bold text-[12px] lg:text-[14px]'>Text Review</p>
-                                    </div>}
-                                {earning.service == 'VideoReview' &&
-                                    <div className='w-full flex flex-row items-center gap-2'>
-                                        <Image src={'/icons/video.svg'} alt='video' width={200} height={200} className='bg-red-500 w-[25px] h-[25px] md:w-[30px] md:h-[30px] p-[3px] rounded-full' />
+                                    </div>
+                                )}
+                                {earning.service === 'VideoReview' && (
+                                    <div className='flex flex-row items-center gap-2'>
+                                        <Image src={'/icons/video.svg'} alt='video review' width={25} height={25} className='bg-red-500 p-[3px] rounded-full' />
                                         <p className='font-bold text-[12px] lg:text-[14px]'>Video Review</p>
-                                    </div>}
-                                {earning.service == 'TextProfileReview' &&
-                                    <div className='w-full flex flex-row items-center gap-2'>
-                                        <Image src={'/icons/account.svg'} alt='video' width={200} height={200} className='bg-orange-500 w-[25px] h-[25px] md:w-[30px] md:h-[30px] p-[3px] rounded-full' />
+                                    </div>
+                                )}
+                                {earning.service === 'TextProfileReview' && (
+                                    <div className='flex flex-row items-center gap-2'>
+                                        <Image src={'/icons/account.svg'} alt='text profile review' width={25} height={25} className='bg-orange-500 p-[3px] rounded-full' />
                                         <p className='font-bold text-[12px] lg:text-[14px]'>Text Profile Review</p>
-                                    </div>}
-                                {earning.service == 'VideoProfileReview' &&
-                                    <div className='w-full flex flex-row items-center gap-2'>
-                                        <Image src={'/icons/video-icon.svg'} alt='video' width={200} height={200} className='bg-green-600 w-[25px] h-[25px] md:w-[30px] md:h-[30px] p-[3px] rounded-full' />
+                                    </div>
+                                )}
+                                {earning.service === 'VideoProfileReview' && (
+                                    <div className='flex flex-row items-center gap-2'>
+                                        <Image src={'/icons/video-icon.svg'} alt='video profile review' width={25} height={25} className='bg-green-600 p-[3px] rounded-full' />
                                         <p className='font-bold text-[12px] lg:text-[14px]'>Video Profile Review</p>
-                                    </div>}
+                                    </div>
+                                )}
                             </td>
                         </tr>
                         <tr>
-                            <td className='font-bold pr-2 bg-white border-b-[3px] border-[#1AAD7A] p-2'>Amount:</td>
-                            <td className='font-bold text-black bg-white border-b-[3px] border-l-[3px] border-[#1AAD7A] p-2'>${(earning?.amount).toFixed(2)}</td>
+                            <td className='font-bold pr-2 bg-white border-[#1AAD7A] border p-2 border-b-[3px] border-r-[3px]'>Amount:</td>
+                            <td className='text-black bg-white border-[#1AAD7A] border p-2 border-b-[3px]'>
+                                <div className='flex flex-col'>
+                                    <div className='flex justify-between'>
+                                        <span className='font-semibold'>Total:</span>
+                                        <span className='font-semibold'>${(earning?.amount / 0.8).toFixed(2)}</span>
+                                    </div>
+                                    <div className='flex justify-between font-bold'>
+                                        <span className='text-gray-600'>20% Fee:</span>
+                                        <span className='text-gray-600'>-${(earning?.amount * 0.2 / 0.8).toFixed(2)}</span>
+                                    </div>
+                                    <hr className='my-2 border-gray-400' />
+                                    <div className='flex justify-between'>
+                                        <span className='font-bold'>Net Amount:</span>
+                                        <span className='font-bold'>${(earning?.amount).toFixed(2)}</span>
+                                    </div>
+                                </div>
+                            </td>
                         </tr>
                         <tr>
-                            <td className='font-bold pr-2 bg-white border-b-[2px] border-[#1AAD7A] p-2'>Date:</td>
-                            <td className='font-bold text-black bg-white border-b-[3px] border-l-[3px] border-[#1AAD7A] p-2'>{formatDate(earning?.createdAt)}</td>
+                            <td className='font-bold pr-2 bg-white border-[#1AAD7A] p-2 border-b-[3px] border-r-[3px]'>Date:</td>
+                            <td className='font-bold text-black bg-white border-[#1AAD7A] border p-2 border-b-[3px]'>{formatDate(earning?.createdAt)}</td>
                         </tr>
                         <tr>
-                            <td className='font-bold pr-2 bg-white rounded-bl-lg p-2'>Time:</td>
-                            <td className='font-bold text-black bg-white rounded-br-lg border-l-[3px] border-[#1AAD7A] p-2'>{formatTime(earning?.createdAt)}</td>
+                            <td className='font-bold pr-2 bg-white border-[#1AAD7A] border p-2 rounded-bl-lg border-b-[3px] border-r-[3px]'>Time:</td>
+                            <td className='font-bold text-black bg-white border-[#1AAD7A] border p-2 rounded-br-lg border-b-[3px]'>{formatTime(earning?.createdAt)}</td>
                         </tr>
                     </tbody>
                 </table>
