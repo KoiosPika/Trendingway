3504c130ca5565ae7f7659dbee2d48b1321002dc
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-06-15 23:59:05 -0700

    redirect signin

--- components/shared/SessionRevoked.tsx
+++ components/shared/SessionRevoked.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 
 const SessionRevoked = () => {
 
@@ -11,18 +12,25 @@ const SessionRevoked = () => {
     useEffect(() => {
 
         const timer = setTimeout(() => {
-            router.replace('/');
-        }, 5000);
+            router.replace('/sign-in?redirectTo=%2Fprofile');
+        }, 10000);
 
         return () => clearTimeout(timer);
     }, []);
 
 
     return (
-        <div>
-            <h1>Session Revoked</h1>
-            <p>Your session has been revoked. Please log in again.</p>
-            <button onClick={() => router.replace('/login')}>Go to Login</button>
+        <div className='w-full flex justify-center items-center bg-white'>
+            <div className='w-full flex flex-col max-w-[950px] justify-center items-center'>
+                <div className='my-3 justify-center items-center flex flex-col w-full'>
+                    <div className='rounded-lg flex flex-col justify-center items-center mt-3 mb-[100px] p-3 w-full lg:w-5/6 bg-white'>
+                        <h2 className='font-bold text-[16px] md:text-[22px]'>Your session has been revoked</h2>
+                        <h3 className='font-bold text-[16px] md:text-[22px]'>Please wait to be redirected</h3>
+                        <Image src={'/images/uncut.png'} alt="logo" className="h-[150px] w-[150px] md:h-[250px] md:w-[250px] animate-spin my-3" height={1000} width={1000} />
+                        <h3 className='font-bold text-[16px] md:text-[22px]'>Loading...</h3>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
