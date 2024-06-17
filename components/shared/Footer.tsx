fdf009de21e748b70e15c4f26eb081a30d4d287f
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-06-17 01:04:41 -0700

    payouts details

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-[#1A1A2E] text-white py-4 px-6"> {/* Use your brand colors */}
+        <footer className="bg-[#1A1A2E] text-white py-2 px-6"> {/* Use your brand colors */}
             <div className="max-w-7xl mx-auto flex flex-col space-y-4 md:space-y-0 md:flex-row md:justify-between md:items-center">
                 <div className="flex flex-col items-center md:items-start">
-                <Image className='h-[70px] w-[70px] bg-black rounded-full' src={'/images/uncut.png'} alt='logo' height={1000} width={1000} />
+                <Image className='h-[45px] w-[45px] bg-black rounded-full' src={'/images/uncut.png'} alt='logo' height={1000} width={1000} />
                 </div>
                 <div className="grid grid-cols-3 md:grid-cols-6 gap-5 text-center text-sm">
                     <a href="/" className="hover:text-yellow-400 font-semibold">Home</a>
@@ -17,7 +17,7 @@ const Footer = () => {
                     <a href="/contact-us" className="hover:text-yellow-400 font-semibold">Contact Support</a>
                 </div>
                 <div className="justify-center mt-4 md:mt-0 hidden md:block">
-                    <Image src={'/images/qr code.PNG'} alt='qr_code' height={200} width={200} className='h-[75px] w-[75px]'/>
+                    <Image src={'/images/qr code.PNG'} alt='qr_code' height={200} width={200} className='h-[45px] w-[45px]'/>
                 </div>
             </div>
         </footer>
