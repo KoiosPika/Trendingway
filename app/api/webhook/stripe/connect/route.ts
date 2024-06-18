3d7db6d7d2b341d9b48a774cbcbba257d77ed329
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-06-17 23:06:58 -0700

    account trial

---
+++ app/api/webhook/stripe/connect/route.ts
@@ -0,0 +1,38 @@
+import stripe from 'stripe'
+import { NextResponse } from 'next/server'
+import { createOrder } from '@/lib/actions/order.actions'
+import UserData from '@/lib/database/models/userData.model'
+
+export async function POST(request: Request) {
+    const body = await request.text()
+
+    const sig = request.headers.get('stripe-signature') as string
+    const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
+
+    let event
+
+    try {
+        event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
+    } catch (err) {
+        return NextResponse.json({ message: 'Webhook error', error: err })
+    }
+
+    // Get the ID and type
+    const eventType = event.type
+
+
+    if (eventType === 'account.updated') {
+        const { id, charges_enabled } = event.data.object
+
+        if (charges_enabled) {
+            await UserData.findOneAndUpdate(
+                { expressAccountID: id },
+                { onboardingCompleted: true }
+            )
+        }
+
+        return NextResponse.json({ message: 'OK' })
+    }
+
+    return new Response('', { status: 200 })
+}
