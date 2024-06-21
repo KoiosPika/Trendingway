a04704b54ff06343290a8c8e4bd12ef6a0ba7ea2
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-06-21 14:31:36 -0700

    home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,7 +14,7 @@ const page = async () => {
       </Head>
       <div className='w-full flex justify-center items-center bg-white'>
         <div className='w-full flex flex-col max-w-[900px] justify-center items-center'>
-          <div className='my-3 justify-center items-center flex flex-col w-full gap-5'>
+          <div className='my-3 justify-center items-center flex flex-col w-full lg:gap-7'>
 
             <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3 mt-5'>
               <div className='bg-yellow-400 w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px] flex flex-col justify-center items-center rounded-[40px] place-self-center'>
@@ -31,7 +31,7 @@ const page = async () => {
               </div>
             </div>
 
-            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3 mt-5'>
+            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3'>
               <div className='bg-white h-[350px] md:flex justify-center items-center hidden'>
                 <Image src={'/images/image_2.png'} alt='image' height={500} width={500} className='rounded-[40px] w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px]' />
               </div>
@@ -49,7 +49,7 @@ const page = async () => {
               </div>
             </div>
 
-            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3 mt-5'>
+            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3'>
               <div className='bg-yellow-400 w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px] flex flex-col justify-center items-center rounded-[40px] place-self-center'>
                 <p className='font-bold text-[30px] md:text-[25px] lg:text-[30px] mr-auto ml-4'>If you have any</p>
                 <p className='font-bold text-[55px] md:text-[45px] lg:text-[55px] ml-auto mr-4'>Questions</p>
@@ -61,6 +61,39 @@ const page = async () => {
               </div>
               <div className='bg-white h-[350px] flex justify-center items-center'>
                 <Image src={'/images/image_3.png'} alt='image' height={500} width={500} className='rounded-[40px] w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px]' />
+              </div>
+            </div>
+
+            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3'>
+              <div className='bg-white h-[350px] md:flex justify-center items-center hidden'>
+                <Image src={'/images/image_4.png'} alt='image' height={500} width={500} className='rounded-[40px] w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px]' />
+              </div>
+              <div className='bg-yellow-400 w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px] flex flex-col justify-center items-center rounded-[40px] place-self-center'>
+                <p className='font-bold text-[30px] md:text-[25px] lg:text-[30px] mr-auto ml-4'>Checkout our</p>
+                <p className='font-bold text-[42px] md:text-[35px] lg:text-[42px] ml-auto mr-4'>Privacy Policy</p>
+                <p className='font-bold text-[18px] md:text-[15px] lg:text-[18px] ml-auto mr-4'>For data information</p>
+                <Link href={'/search'} className='ml-auto mr-[30px] mt-5 flex justify-center gap-2 items-center bg-black px-3 py-1 rounded-md'>
+                  <Image src={'/icons/privacy.svg'} alt='guide' height={20} width={20} />
+                  <p className='text-white font-semibold text-[17px]'>Privacy Policy</p>
+                </Link>
+              </div>
+              <div className='bg-white h-[350px] flex justify-center items-center md:h-[0px]'>
+                <Image src={'/images/image_4.png'} alt='image' height={500} width={500} className='rounded-[40px] w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px] block md:hidden' />
+              </div>
+            </div>
+
+            <div className='grid md:grid-cols-2 grod-cols-1 w-10/12 gap-5 md:gap-3'>
+              <div className='bg-yellow-400 w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px] flex flex-col justify-center items-center rounded-[40px] place-self-center'>
+                <p className='font-bold text-[30px] md:text-[25px] lg:text-[30px] mr-auto ml-4'>Please read our</p>
+                <p className='font-bold text-[44px] md:text-[38px] lg:text-[44px] ml-auto mr-4'>Terms of Use</p>
+                <p className='font-bold text-[18px] md:text-[15px] lg:text-[18px] ml-auto mr-4'>Before using our platform</p>
+                <Link href={'/'} className='ml-auto mr-[30px] mt-5 flex justify-center gap-2 items-center bg-black px-3 py-1 rounded-md'>
+                  <Image src={'/icons/list-white.svg'} alt='guide' height={20} width={20} />
+                  <p className='text-white font-semibold text-[17px]'>Terms of Use</p>
+                </Link>
+              </div>
+              <div className='bg-white h-[350px] flex justify-center items-center'>
+                <Image src={'/images/image_5.png'} alt='image' height={500} width={500} className='rounded-[40px] w-[350px] h-[350px] md:h-[300px] md:w-[300px] lg:h-[350px] lg:w-[350px]' />
               </div>
             </div>
           </div>
