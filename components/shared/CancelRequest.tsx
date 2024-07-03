4f8806bffff29749d4e8b546a46d474b76b7dbb9
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-03 16:18:29 -0700

    footer ui

--- components/shared/CancelRequest.tsx
+++ components/shared/CancelRequest.tsx
@@ -26,7 +26,7 @@ const CancelRequest = ({ request }: { request: string }) => {
     }
 
     return (
-        <Button className='bg-red-500 w-1/6 flex flex-row items-center justify-center gap-2 py-1 rounded-lg mt-4 mb-2 hover:cursor-default hover:bg-yellow-400' onClick={handleCancelingRequest}>
+        <Button className='bg-red-500 w-1/6 flex flex-row items-center justify-center gap-2 py-1 rounded-lg mt-4 mb-2 hover:cursor-pointer hover:bg-red-500' onClick={handleCancelingRequest}>
             {!loading && <p className='text-[13px] md:text-[16px] text-white font-bold'>X</p>}
             {loading && <Image src={'/icons/spinner.svg'} alt='spinner' height={40} width={40} className=' animate-spin'/>}
         </Button>
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -3,21 +3,14 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <footer className="bg-[#1A1A2E] text-white py-2 px-6"> {/* Use your brand colors */}
-            <div className="max-w-7xl mx-auto flex flex-col space-y-4 md:space-y-0 md:flex-row md:justify-between md:items-center">
-                <div className="flex flex-col items-center md:items-start">
-                <Image className='h-[45px] w-[45px] bg-black rounded-full' src={'/images/uncut.png'} alt='logo' height={1000} width={1000} />
-                </div>
-                <div className="grid grid-cols-3 md:grid-cols-6 lg:gap-5 gap-2 text-center text-[10px] lg:text-[13px]">
+        <footer className="bg-[#1A1A2E] text-white py-2 px-6 flex justify-center items-center">
+            <div className="flex md:flex-row flex-col items-center justify-center gap-7 w-full max-w-[800px]">
+                <div className="text-[12px] lg:text-[13px] flex flex-row gap-7 mt-3 md:mt-0">
                     <a href="/" className="hover:text-yellow-400 font-semibold">Home</a>
-                    <a href="/profile" className="hover:text-yellow-400 font-semibold">Profile</a>
-                    <a href="/wallet" className="hover:text-yellow-400 font-semibold">Wallet</a>
                     <a href="/privacy" className="hover:text-yellow-400 font-semibold">Privacy Policy</a>
                     <a href="/terms" className="hover:text-yellow-400 font-semibold">Terms of Use</a>
                 </div>
-                <div className="justify-center mt-4 md:mt-0 hidden lg:block">
-                    <Image src={'/images/qr code.PNG'} alt='qr_code' height={200} width={200} className='h-[45px] w-[45px]'/>
-                </div>
+                <p className='font-semibold text-[13px]'>Contact: support@insightend.com</p>
             </div>
         </footer>
     )
