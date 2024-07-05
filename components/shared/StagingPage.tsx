7d549d512e959b472e49962b7240b03849bac13f
KoiosPika <109259191+KoiosPika@users.noreply.github.com>
2024-07-05 00:32:30 -0700

    sign up redirecting

--- components/shared/StagingPage.tsx
+++ components/shared/StagingPage.tsx
@@ -21,7 +21,7 @@ const StagingPage = () => {
         <div className='flex flex-col justify-center items-center gap-3'>
             <Image src={'/images/uncut.png'} alt='logo' height={200} width={200} />
             <p className='text-[16px] font-semibold'>Please wait</p>
-            <p className='text-[16px] font-semibold'>We're setting up your account</p>
+            <p className='text-[16px] font-semibold'>{`We're`} setting up your account</p>
             <Image src={'/icons/spinner.svg'} alt='spinner' height={30} width={30} className=' animate-spin' />
         </div>
     )
